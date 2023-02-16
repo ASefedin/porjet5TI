3323e0b7aef87337f65320bd15d835d6cadc68e0
Ajdini Sefedin <180000@CCM-Asty-Moulin.local>
2023-02-16 14:23:09 +0100

    test inscription

--- controllers/utilisateursControllers.php
+++ controllers/utilisateursControllers.php
@@ -5,6 +5,10 @@ $uri = $_SERVER["REQUEST_URI"];
 if ($uri === "/connexion") {
     require_once "template/utilisateurs/connexion.php";
 } elseif ($uri === "/inscription" ) {
+    var_dump($_POST);
+    if ($_POST["btnEnvoi"]) {
+        var_dump("formulaire envoye");
+    }
     require_once "template/utilisateurs/inscriptionOrEditProfil.php";
 }elseif ($uri === "/profil" ) {
     require_once "template/utilisateurs/profil.php";
